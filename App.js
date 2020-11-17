3646270a07942b0d99df10b16a3af3d9030e1091
Teerasej <teerasej@gmail.com>
2020-11-17 20:55:31 +0700

    implement react hook

--- App.js
+++ App.js
@@ -1,14 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Goodbye from './Goodbye';
+import Hello from './Hello';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [isFirstTime, setFirstTime] = useState(true);
+  const [isSignedIn, setSignIn] = useState(false);
+  
+  let popUpAlert = () => {
+    Alert.alert('oops...', 'you do something');
+  }
+
+  let signIn = () => {
+    setFirstTime(false);
+    setSignIn(true);
+  }
+
+  let signOut = () => {
+    setSignIn(false);
+  }
+
+ 
+    let username = 'พล';
+    let website = 'nextflow.in.th'
+
+    let buttonComponent;
+    let wordComponent;
+
+
+    if (isFirstTime) {
+      buttonComponent = (
+        <Button
+          title="ลงชื่อเข้าใช้"
+          onPress={signIn}
+        />
+      )
+    } else {
+      if (isSignedIn) {
+        buttonComponent = (
+          <Button
+            title="ออกจากระบบ"
+            onPress={signOut}
+          />
+        )
+        wordComponent = (
+          <Hello name="พล" website="nextflow.in.th" />
+        )
+      } else {
+        buttonComponent = (
+          <Button
+            title="ลงชื่อเข้าใช้"
+            onPress={signIn}
+          />
+        )
+        wordComponent = (
+          <Goodbye name="พล" website="nextflow.in.th" />
+        )
+      }
+    }
+
+    return (
+      <View style={styles.container}>
+        {wordComponent}
+        {buttonComponent}
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
